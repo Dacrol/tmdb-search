7eb0f804603786b98aa3bae9ca631c939e86eabf
Jonas Ahrens <jonas.ahrens@gmail.com>
2023-11-26 00:20:15 +0100

    Add API proxy

---
+++ server/server.ts
@@ -0,0 +1,37 @@
+// express proxy server to the TMDB API. Used to keep the API key secret.
+
+import express from 'express';
+import axios from 'axios';
+
+require('dotenv').config({
+  path: './.env',
+});
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+const TMDB_API_KEY = process.env.TMDB_API_KEY;
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+app.get('/api/*', (req, res) => {
+  const url = `${BASE_URL}${req.url.replace('/api', '')}`;
+  console.log(url);
+
+  axios
+    .get(url, {
+      headers: {
+        Authorization: `Bearer ${TMDB_API_KEY}`,
+        accept: 'application/json',
+      },
+    })
+    .then(data => {
+      res.send(data.data.results);
+    })
+    .catch(error => res.send(error));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
